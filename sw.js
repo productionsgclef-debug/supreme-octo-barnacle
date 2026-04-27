ccbcbffd073275f74dfb7217e3d55e1e477552a4
productionsgclef-debug <productionsgclef@gmail.com>
2026-04-27 17:20:36 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('octo-cache-v1').then((cache) => cache.addAll(['/'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
